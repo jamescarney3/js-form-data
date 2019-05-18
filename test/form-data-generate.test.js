969690d2a51840440e081ae1f78316606226b27c
James Carney <jmcarneyiii@gmail.com>
2019-05-17 20:42:34 -0400

    add generate method and tests

---
+++ test/form-data-generate.test.js
@@ -0,0 +1,24 @@
+'use-strict';
+
+import JSONFormData from '../src/form-data';
+
+
+test('returns a FormData object', () => {
+  const formData = new JSONFormData();
+
+  expect(formData.generate() instanceof FormData).toBe(true);
+});
+
+test('appends each element of length == 1 array type value to returned FormData object', () => {
+  const formData = new JSONFormData();
+
+  formData._data = { foo: ['bar'] };
+  expect(formData.generate().get('foo')).toEqual('bar');
+});
+
+test('appends each element of length > 1 array type value to returned FormData object', () => {
+  const formData = new JSONFormData();
+
+  formData._data = { foo: ['bar', 'baz', 'qux'] };
+  expect(formData.generate().getAll('foo')).toEqual(['bar', 'baz', 'qux']);
+});
