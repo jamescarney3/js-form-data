f905a7bc59c83564162824110b49098f610eb403
James Carney <james.carney@vshift.com>
2019-05-22 12:20:37 -0400

    Add assertions for append(obj) tests

--- test/form-data-append.test.js
+++ test/form-data-append.test.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV !== 'debug') {
 }
 
 
-test('appends k/v pair from (<str>, <_>) args to _data instance variable', () => {
+test('appends new k/v pair from (<str>, <_>) args to _data instance variable', () => {
   const formData = new JSFormData();
   
   formData.append('foo', 'bar');
@@ -32,7 +32,15 @@ test('appends k/v pair from (<str>, <_>) args to _data instance variable', () =>
 });
 
 
-test.todo('appends k/v pair(s) from <obj> arg to _data instance variable');
+test('appends new k/v pair(s) from <obj> arg to _data instance variable', () => {
+  const formData = new JSFormData();
+  
+  formData.append({ foo: 'bar' });
+  expect(formData._data).toHaveProperty('foo', ['bar']);
+  formData.append({ baz: 'qux', quux: 'quuz' });
+  expect(formData._data).toHaveProperty('baz', ['qux']);
+  expect(formData._data).toHaveProperty('quux', ['quuz']);
+});
 
 
 test('throws exception when 1st positional arg is not an object and 2nd positional arg is not passed', () => {
@@ -46,20 +54,27 @@ test('throws exception when 1st positional arg is not an object and 2nd position
 
 test('updates existing k/v pair from (<str>, <_>) args when key already exists on _data instance variable', () => {
   const formData = new JSFormData();
-
   formData._data = { foo: ['bar'] };
+  
   formData.append('foo', 'baz');
   expect(formData._data).toHaveProperty('foo', ['bar', 'baz']);
 });
 
 
-test.todo('updates existing k/v pair(s) from <obj> arg when one+ key exists on _data instance variable');
+test('updates existing k/v pair(s) from <obj> arg when one+ key exists on _data instance variable', () => {
+  const formData = new JSFormData();
+  formData._data = { foo: ['bar'] };
+  
+  formData.append({ foo: 'baz', qux: 'quux' });
+  expect(formData._data).toHaveProperty('foo', ['bar', 'baz']);
+  expect(formData._data).toHaveProperty('qux', ['quux']);
+});
 
 
 test('appends k/v pair from (<str>, <File>, <str>) args with filename when value is a Blob', () => {
   const formData = new JSFormData();
-  
   const blob = new Blob([JSON.stringify({ foo: 'bar' })], { type: 'application/json'});
+  
   formData.append('blob', blob, 'test-blob');
   expect(formData._data['blob'][0]).toHaveProperty('name', 'test-blob');
 });
@@ -75,8 +90,8 @@ test('appends k/v pair without provided filename when value is not a Blob instan
 
 test('clones blob to add to _data instance variable', () => {
   const formData = new JSFormData();
-  
   const blob = new Blob([JSON.stringify({ foo: 'bar' })], { type: 'application/json'});
+  
   formData.append('blob', blob);
   expect(formData._data['blob'][0]).not.toBe(blob);
   expect(formData._data['blob'][0].size).toEqual(blob.size);
