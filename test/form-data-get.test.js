c2ad3bd23cdabe8a2137a3362c7d45fca1975f48
James Carney <james.carney@vshift.com>
2019-05-24 14:59:25 -0400

    update readme, get test, get method

--- test/form-data-get.test.js
+++ test/form-data-get.test.js
@@ -3,6 +3,41 @@
 import JSFormData from '../src/form-data';
 
 
+if (process.env.NODE_ENV !== 'debug') {
+  beforeAll(() => {
+    // Create a spy on console (console.log in this case) and provide some mocked implementation
+    // In mocking global objects it's usually better than simple `jest.fn()`
+    // because you can `unmock` it in clean way doing `mockRestore` 
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  afterAll(() => {
+    // Restore mock after all tests are done, so it won't affect other test suites
+    console.error.mockRestore();
+  });
+
+  afterEach(() => {
+    // Clear mock (all calls etc) after each test. 
+    // It's needed when you're using console somewhere in the tests so you have clean mock each time
+    console.error.mockClear();
+  });
+}
+
+
+test('throws exception with unrecognized param signature passed', () => {
+  const formData = new JSFormData();  
+  const errorSpy = jest.spyOn(global.console, 'error');
+
+  // see method for valid param signatures
+  formData.get();
+  formData.get(1);
+  formData.get(true);
+  formData.get(['foo']);
+  formData.get({ foo: 'bar' });
+  expect(errorSpy).toHaveBeenCalledTimes(5);
+});
+
+
 test('returns the value of a present key', () => {
   const formData = new JSFormData();
   
